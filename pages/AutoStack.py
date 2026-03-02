26be7a22b2cf685ec4f4319af38777e3e23ad585
chrisnew143-gif <chrisnew143@gmail.com>
2026-03-02 11:04:52 +0800

    Update AutoStack.py

--- pages/AutoStack.py
+++ pages/AutoStack.py
@@ -12,7 +12,7 @@ supabase = get_supabase()
 # ======================================================
 # PAGE CONFIG
 # ======================================================
-st.set_page_config(page_title="Pickleball Auto Stack TiraDinks Official", page_icon="🎾", layout="wide")
+st.set_page_config(page_title="Pickleball Auto Stack LPC Official", page_icon="🎾", layout="wide")
 
 st.markdown("""
 <style>
@@ -38,10 +38,10 @@ a[href*="github.com/streamlit"]{display:none!important;}
 # =========================
 col1, col2, col3 = st.columns([1,2,1])
 with col2:
-    st.image("TDphoto.jpg", width=300)
+    st.image("lpcphoto.jpg", width=300)
 
-st.title("🎾 Pickleball Auto Stack TiraDinks Official")
-st.caption("WE CAMED WE DINKED!")
+st.title("🎾 Pickleball Auto Stack LPC Official")
+st.caption("LET'S PLAY!")
 
 # ======================================================
 # HELPERS
